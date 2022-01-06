43921aa4e4f226a5f0d55fc1c39a6819911c4bfd
Zhou YuXi <zhouyuxi.abc@gmail.com>
2022-01-06 12:30:05 +0800

    primary func completed

--- widget.py
+++ widget.py
@@ -3,10 +3,10 @@ import sys
 
 from PySide6.QtCore import *
 from PySide6.QtGui import QColor, QGuiApplication, QPixmap, QAction, QIcon, QMouseEvent, QPaintEvent, QPainter, QPen, \
-    QCursor, QScreen
-from PySide6.QtWidgets import QApplication, QWidget, QLabel, QSystemTrayIcon, QMenu, QFileDialog
+    QCursor, QScreen, QShortcut, QKeySequence
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QSystemTrayIcon, QMenu, QFileDialog, QPushButton
 
-grey_color = "#bdc3c7"
+grey_color = QColor(0, 0, 0, 200)
 white_color = QColor(160, 160, 160, 200)
 brush_color = QColor(128, 128, 255, 100)
 
@@ -15,7 +15,7 @@ class ScreenShotWidget(QWidget):
     def __init__(self, app: QApplication, width: int, height: int):
         super(ScreenShotWidget, self).__init__()
         self.setGeometry(0, 0, width, height)
-        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(Qt.FramelessWindowHint)
         self.label = QLabel(parent=self)
         self.canvas = QPixmap(width, height)
         self.canvas.fill(white_color)
@@ -35,17 +35,25 @@ class ScreenShotWidget(QWidget):
         qp.setBrush(brush_color)
         rect = QRectF(self.beginX, self.beginY, self.endX - self.beginX, self.endY - self.beginY)
         qp.drawRect(rect)
+        qp.fillRect(rect, grey_color)
+
+    def computeRect(self):
+        x = min(self.beginX, self.endX)
+        y = min(self.beginY, self.endY)
+        xDis = abs(self.endX - self.beginX)
+        yDis = abs(self.endY - self.beginY)
+        return x, y, xDis, yDis
 
     @Slot()
     def shot(self):
         self.isPopup = False
         self.setWindowOpacity(0)
         screen = QApplication.primaryScreen()
-        x = min(self.beginX, self.endX)
-        y = min(self.beginY, self.endY)
-        xDis = abs(self.endX - self.beginX)
-        yDis = abs(self.endY - self.beginY)
-        print(x, y, xDis, yDis)
+        re = self.computeRect()
+        x = re[0]
+        y = re[1]
+        xDis = re[2]
+        yDis = re[3]
         pix = screen.grabWindow(0, x, y, xDis, yDis)
         QApplication.clipboard().setPixmap(pix)
 
@@ -53,11 +61,15 @@ class ScreenShotWidget(QWidget):
         self.isPopup = False
         self.beginY = self.beginX = self.endY = self.endX = 0
         self.update()
+        QApplication.setOverrideCursor(QCursor(Qt.ArrowCursor))
         self.hide()
 
     @Slot()
     def exit(self):
-        QApplication.exit(0)
+        self.isPopup = False
+        self.beginY = self.beginX = self.endY = self.endX = 0
+        QApplication.setOverrideCursor(QCursor(Qt.ArrowCursor))
+        self.hide()
 
     def mouseMoveEvent(self, e: QMouseEvent):
         if not self.isReleased:
@@ -69,25 +81,22 @@ class ScreenShotWidget(QWidget):
     def saveToFile(self):
         self.setWindowOpacity(0)
         screen = QApplication.primaryScreen()
-        x = min(self.beginX, self.endX)
-        y = min(self.beginY, self.endY)
-        xDis = abs(self.endX - self.beginX)
-        yDis = abs(self.endY - self.beginY)
-        print(x, y, xDis, yDis)
+        re = self.computeRect()
+        x = re[0]
+        y = re[1]
+        xDis = re[2]
+        yDis = re[3]
         pix = screen.grabWindow(0, x, y, xDis, yDis)
+        self.hide()
+        filename, _ = QFileDialog.getSaveFileName(self, caption="choose location and file name",
+                                                  filter="PNG(*.png);; JPEG(*.jpg)")
 
-        img, _ = QFileDialog.getSaveFileName(self, "Salvar Arquivo",
-                                             filter="PNG(*.png);; JPEG(*.jpg)")
-
-        if img[-3:] == "png":
-            pix.save(img, "png")
-        elif img[-3:] == "jpg":
-            pix.save(img, "jpg")
+        if filename[-3:] == "png":
+            pix.save(filename, "png")
+        elif filename[-3:] == "jpg":
+            pix.save(filename, "jpg")
         self.beginY = self.beginX = self.endY = self.endX = 0
         self.update()
-        self.hide()
-
-        print('save to file')
         QApplication.setOverrideCursor(QCursor(Qt.CrossCursor))
         self.isPopup = False
 
@@ -119,10 +128,23 @@ class ScreenShotWidget(QWidget):
         self.isPopup = False
 
 
+class MainWindow(QWidget):
+    def __init__(self, window):
+        super(MainWindow, self).__init__()
+        self.rectBtn = QPushButton("rect shot", self)
+        self.rectBtn.show()
+        self.screenWindow = window
+        self.rectBtn.clicked.connect(self.showWindow)
+
+    @Slot()
+    def showWindow(self):
+        self.screenWindow.show()
+        self.window().hide()
+        QApplication.setOverrideCursor(Qt.CrossCursor)
+
+
 if __name__ == "__main__":
     app = QApplication([])
-    # app.setQuitOnLastWindowClosed(False)
-
     icon = QIcon("./assets/img/icon.png")
     tray = QSystemTrayIcon(icon, app)
     tray.setVisible(True)
@@ -140,10 +162,13 @@ if __name__ == "__main__":
 
     curr_size = QGuiApplication.primaryScreen().size()
     window = ScreenShotWidget(app, curr_size.width(), curr_size.height())
-    showWindow.triggered.connect(lambda: window.show())
-    tray.connect(QSystemTrayIcon.ActivationReason, lambda : menu.show())
+    showWindow.triggered.connect(lambda: (window.show(), QApplication.setOverrideCursor(Qt.CrossCursor)))
 
-    # window.showFullScreen()
+    showWindow.setShortcut(QKeySequence("Ctrl+Shift+A"))
+
     window.resize(curr_size.width(), curr_size.height())
-    window.show()
+    mainWindow = MainWindow(window)
+    window.hide()
+    mainWindow.show()
+    QApplication.setOverrideCursor(Qt.ArrowCursor)
     sys.exit(app.exec())
